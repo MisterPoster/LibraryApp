3d73a4d45ee2b1d2fe45ad9f6d96f87007a8933f
SammyS <23059189+MisterPoster@users.noreply.github.com>
2019-08-26 08:20:23 -0700

    Book.java

---
+++ Book.java
@@ -0,0 +1,13 @@
+public class Book
+{
+    int pages = 0, idNumber = 0;
+    String title = "", author = "";
+    boolean ifAvailable = false;
+}
+
+//VARIABLES
+//CONSTRUCTOR
+//GETTERS
+//SETTERS
+//BRAIN METHODS
+//TO-STRING
